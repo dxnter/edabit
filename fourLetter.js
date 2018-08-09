a1f33d683542bcd207a3618fab9c90790d614bdd
Danny <contact@danfoster.io>
2018-08-09 16:08:01 -0500

    New challenges

---
+++ fourLetter.js
@@ -0,0 +1,3 @@
+function isFourLetters(arr) {
+  return arr.filter(str => str.length === 4);
+}
---
+++ repeatItem.js
@@ -0,0 +1,7 @@
+function repeat(item, times) {
+  const result = [];
+  for (let i = times; i > 0; i--) {
+    result.push(item);
+  }
+  return result;
+}
